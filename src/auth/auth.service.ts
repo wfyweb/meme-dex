80dfa87e530bd73a6652cbf4240dd1a210928641
wangfangyuan <webwfy@163.com>
2024-12-03 16:42:56 +0800

    fix: verifyMessage find account error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -68,107 +68,117 @@ export class AuthService {
     const { address, message, signature } = CreateAddressDto
     const nonce = Math.floor(Math.random() * 1000000) + ''
     console.log(address, message, signature)
-    const addressRecord = await this.prisma.account.findFirst({
-      where: {
-        uniqueId: address,
-      },
-    });
-    if (!addressRecord) {
-      // 未查询到该用户
-      // throw new HttpException('The user was not found.', 400);
+    try {
+      const addressRecord = await this.prisma.account.findFirst({
+        where: {
+          uniqueId: address,
+        },
+      });
+      if (!addressRecord) {
+        // 未查询到该用户
+        // throw new HttpException('The user was not found.', 400);
+        return {
+          code: 400,
+          msg: 'The user was not found',
+        }
+      }
+
+
+      // 是否Solana链钱包
+      if (this.isValidSolanaAddress(address)) {
+        console.log('start==> sol', addressRecord, message)
+        try {
+          const pubKey = new PublicKey(address);
+          const signatureBuffer = new Uint8Array(Buffer.from(signature, 'base64')); // base64转化为Uint8Array格式
+          // const _message  = `meme-dex wants you to sign in with your Solana account:\nGdS4cqHxVfEeQuoKc8CtXhYB1KFQRUiyyuFz52yT5on\nNonce: ${addressRecord}`
+          const messageBytes = decodeUTF8(message);
+          const isValid = sign.detached.verify(
+            messageBytes,
+            signatureBuffer,
+            pubKey.toBytes() // Uint8Array
+          );
+          if (isValid) {
+            // 查到用户，更新nonce, type
+            await this.prisma.$transaction(async (prisma) => {
+              return await Promise.all([
+                prisma.user.update({
+                  where: {
+                    id: addressRecord.userId
+                  },
+                  data: {
+                    nonce
+                  }
+                }),
+                prisma.account.update({
+                  where: {
+                    id: addressRecord.id,
+                  },
+                  data: {
+                    type: 'wallet:sol'
+                  }
+                }),
+              ])
+            })
+            return {
+              code: 0,
+              msg: 'Login successful',
+            }
+          } else {
+            throw new HttpException('verify message fail.', 400);
+          }
+        } catch (error) {
+
+          // throw new HttpException(error, 400);
+          return {
+            code: 400,
+            msg: error,
+          }
+        }
+      } else if (ethers.utils.isAddress(address)) {
+        // eth 钱包
+        try {
+          const recoveredAddress = ethers.utils.verifyMessage(message, signature)
+          if (recoveredAddress.toLowerCase() === address.toLowerCase()) {
+            // 查到用户，更新nonce
+            await this.prisma.$transaction(async (prisma) => {
+              return await Promise.all([
+                prisma.user.update({
+                  where: {
+                    id: addressRecord.userId
+                  },
+                  data: {
+                    nonce
+                  }
+                }),
+                prisma.account.update({
+                  where: {
+                    id: addressRecord.id,
+                  },
+                  data: {
+                    type: 'wallet:eth'
+                  }
+                }),
+              ])
+            })
+            return {
+              code: 0,
+              msg: 'Login successful',
+            }
+          } else {
+            throw new HttpException('verify message fail.', 400);
+          }
+        } catch (error) {
+          throw new HttpException(error, 400);
+        }
+      } else {
+        throw new HttpException('The wallet address is error.', 400);
+      }
+
+    } catch (error) {
       return {
         code: 400,
         msg: 'The user was not found',
       }
-    }
-    // 是否Solana链钱包
-    if (this.isValidSolanaAddress(address)) {
-      console.log('start==> sol',addressRecord, message)
-      try {
-        const pubKey = new PublicKey(address);
-        const signatureBuffer = new Uint8Array(Buffer.from(signature, 'base64')); // base64转化为Uint8Array格式
-        // const _message  = `meme-dex wants you to sign in with your Solana account:\nGdS4cqHxVfEeQuoKc8CtXhYB1KFQRUiyyuFz52yT5on\nNonce: ${addressRecord}`
-        const messageBytes = decodeUTF8(message);
-        const isValid = sign.detached.verify(
-          messageBytes,
-          signatureBuffer,
-          pubKey.toBytes() // Uint8Array
-        );
-        if (isValid) {
-          // 查到用户，更新nonce, type
-          await this.prisma.$transaction(async (prisma) => {
-            return await Promise.all([
-              prisma.user.update({
-                where: {
-                  id: addressRecord.userId
-                },
-                data: {
-                  nonce
-                }
-              }),
-              prisma.account.update({
-                where: {
-                  id: addressRecord.id,
-                },
-                data: {
-                  type: 'wallet:sol'
-                }
-              }),
-            ])
-          })
-          return {
-            code: 0,
-            msg: 'Login successful',
-          }
-        } else {
-          throw new HttpException('verify message fail.', 400);
-        }
-      } catch (error) {
-        
-        // throw new HttpException(error, 400);
-        return {
-          code: 400,
-          msg: error,
-        }
-      }
-    } else if (ethers.utils.isAddress(address)) {
-      // eth 钱包
-      try {
-        const recoveredAddress = ethers.utils.verifyMessage(message, signature)
-        if (recoveredAddress.toLowerCase() === address.toLowerCase()) {
-          // 查到用户，更新nonce
-          await this.prisma.$transaction(async (prisma) => {
-            return await Promise.all([
-              prisma.user.update({
-                where: {
-                  id: addressRecord.userId
-                },
-                data: {
-                  nonce
-                }
-              }),
-              prisma.account.update({
-                where: {
-                  id: addressRecord.id,
-                },
-                data: {
-                  type: 'wallet:eth'
-                }
-              }),
-            ])
-          })
-          return {
-            code: 0,
-            msg: 'Login successful',
-          }
-        } else {
-          throw new HttpException('verify message fail.', 400);
-        }
-      } catch (error) {
-        throw new HttpException(error, 400);
-      }
-    }else{
-      throw new HttpException('The wallet address is error.', 400);
     }
   }
 }
